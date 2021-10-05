568ef8c2b756f2ef08017eabf7965575e70a5eec
pierre <pnavarin@gmail.com>
2021-10-05 14:31:52 -0400

    Setup board

--- skeleton/src/ttt-view.js
+++ skeleton/src/ttt-view.js
@@ -1,7 +1,20 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    const ul = document.createElement('ul');
+    for(let i = 0; i < 3; i++){
+      for (let j = 0; j < 3; j++) {
+      const li = document.createElement('li');
+      li.setAttribute('data-pos', [i, j].join(', '));
+      ul.appendChild(li);
+      }
+    }
+    
+  }
   
   bindEvents() {}
 
